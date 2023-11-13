b6cbb771fbe5c2a23533931dd5e08a59b3ae0474
shinnthant2004 <shinthantequi@gmail.com>
2023-11-13 17:18:47 +0630

    add:toast-on-add-to-cart

--- app/(routes)/product/[slug]/components/AddToCart.tsx
+++ app/(routes)/product/[slug]/components/AddToCart.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useToast } from "@/components/ui/use-toast";
 import useCart from "@/lib/hooks/useCart";
 import { PlusIcon } from "@radix-ui/react-icons";
 
@@ -9,10 +10,19 @@ interface Props {
 
 export default function AddToCart({ productId }: Props) {
   const { addToCart } = useCart();
+  const { toast } = useToast();
 
   return (
     <button
-      onClick={() => addToCart(productId)}
+      onClick={() => {
+        addToCart(productId);
+        toast({
+          type: "background",
+          color: "black",
+          title: "Added to the cart.",
+          description: "View your cart list.",
+        });
+      }}
       className="w-full relative h-12 rounded-full bg-button_two text-white flex items-center justify-center"
     >
       <PlusIcon className="absolute left-5" />
